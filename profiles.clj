fdadca0e6aa1746a85ea4b69b3e31d7522e63736
Chris Koehnen <chris.koehnen@deluxe.com>
2016-05-09 09:17:39 -0500

    initial commit

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+
+{:repl {:plugins [[cider/cider-nrepl "0.12.0"]]}
+ :user {:plugins [[lein-ancient "0.6.10"]
+		  [lein-bikeshed "0.3.0"]
+		  [jonase/eastwood "0.2.3"]
+		  [lein-kibit "0.1.2"]
+		  [lein-cloverage "1.0.6"]]}}
