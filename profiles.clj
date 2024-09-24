4d5725149c01ec676ff089e4bf669a1963c9fa39
Chris Koehnen <crkoehnen@gmail.com>
2024-09-24 16:44:52 -0500

    Adding koans, inc nrepl, sorting plugins

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,10 @@
 
-{:repl {:plugins [[cider/cider-nrepl "0.50.1"]]}
- :user {:plugins [[lein-ancient "0.7.0"]
-		  [lein-bikeshed "0.5.2"]
+{:repl {:plugins [[cider/cider-nrepl "0.50.2"]]}
+ :user {:plugins [
 		  [jonase/eastwood "1.4.3"]
+		  [lein-bikeshed "0.5.2"]
+		  [lein-cloverage "1.2.4"]
 		  [lein-kibit "0.1.11"]
-		  [lein-cloverage "1.2.4"]]}}
+                  [koan/lein-template "0.1.3"]
+                  [lein-ancient "0.7.0"]
+                  ]}}
